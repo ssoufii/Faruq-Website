6f8257229c44f8a02908339d391e13b0d5fc3b51
Salim Soufi <salim_soufi@icloud.com>
2024-10-12 23:19:51 -0600

    added correct files

--- js/main.js
+++ js/main.js
@@ -1,277 +1,150 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
+(function ($) {
+    "use strict";
 
-(function($) {
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
 
-	"use strict";
+    // Initiate the wowjs // dont remove this or the planet explodes into 300000 pieces 
+    new WOW().init();
 
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
 
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
 
-	var fullHeight = function() {
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
 
-	};
-	fullHeight();
+    // Prayer Times
 
-	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
+    function iqamah(time, n) {
+        let [hours, minutes] = time.split(':').map(Number);
+        minutes += n; 
 
-	// Scrollax
-   $.Scrollax();
+        while (minutes >= 60) {
+            minutes -= 60;
+            hours += 1;
+        }
 
+        const period = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12; 
 
-
-   // Burger Menu
-	var burgerMenu = function() {
-
-		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
-
-			event.preventDefault();
-
-			if ( $('#ftco-nav').is(':visible') ) {
-				$(this).removeClass('active');
-			} else {
-				$(this).addClass('active');	
-			}
-
-			
-			
-		});
-
-	};
-	burgerMenu();
-
-
-	var onePageClick = function() {
-
-
-		$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
-	    event.preventDefault();
-
-	    var href = $.attr(this, 'href');
-
-	    $('html, body').animate({
-	        scrollTop: $($.attr(this, 'href')).offset().top - 70
-	    }, 500, function() {
-	    	// window.location.hash = href;
-	    });
-		});
-
-	};
-
-	onePageClick();
-	
-
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-	};
-	carousel();
-
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
-	});
-
-
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
-	});
-
-	// scroll
-	var scrollWindow = function() {
-		$(window).scroll(function(){
-			var $w = $(this),
-					st = $w.scrollTop(),
-					navbar = $('.ftco_navbar'),
-					sd = $('.js-scroll-wrap');
-
-			if (st > 150) {
-				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
-				}
-			} 
-			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
-				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
-				}
-				
-				if(sd.length > 0) {
-					sd.addClass('sleep');
-				}
-			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
-				}
-				if(sd.length > 0) {
-					sd.removeClass('sleep');
-				}
-			}
-		});
-	};
-	scrollWindow();
-
-	
-
-	var counter = function() {
-		
-		$('#section-counter, .hero-wrap, .ftco-counter, .ftco-about').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-						console.log(num);
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
-				
-			}
-
-		} , { offset: '95%' } );
-
-	}
-	counter();
-
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.ftco-animate').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .ftco-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn ftco-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft ftco-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight ftco-animated');
-							} else {
-								el.addClass('fadeInUp ftco-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
-
-	// magnific popup
-	$('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    fixedContentPos: true,
-    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      verticalFit: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
+        return `${hours}:${minutes.toString().padStart(2, '0')} ${period}`;
     }
-  });
+    const imageMap = {
+        "Fajr": "img/icon/fajr.png",
+        "Dhuhr": "img/icon/dhuhr.png",
+        "Asr": "img/icon/asr.png",
+        "Maghrib": "img/icon/maghreb.png",
+        "Isha": "img/icon/isha.png"
+    };
+    function fetchAndDisplayPrayerTimes() {
+        const prayerTimesContainer = document.getElementById('prayer-times-container');
+        const topBarPrayerTimes = document.getElementById('top-bar-prayer-times');
 
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
+        fetch('http://api.aladhan.com/v1/timingsByCity?city=Edmonton&country=Canada&method=2')
+            .then(response => response.json())
+            .then(data => {
+                const timings = data.data.timings;
+                const prayerNames = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'];
+                
+                let cardsHTML = '';
+                let topBarHTML = '';
+                prayerNames.forEach((prayer, index) => {
+                    const time12h = convertTo12HourFormat(timings[prayer]);
+                    const militarytime = timings[prayer];
+                    cardsHTML += `
+                        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="${0.1 * (index + 1)}s">
+                          <div class="service-item rounded h-100 p-5">
+                              <div class="d-flex flex-column align-items-center ms-n5 mb-4">
+                                  <div class="service-icon flex-shrink-0 bg-primary rounded mb-4">
+                                      <img class="img-fluid" src=${imageMap[prayer]} alt="" />
+                                  </div>
+                                  <h4 class="mb-0">${prayer}</h4>
+                                  <h4 class="mb-0 text-muted" style="opacity: 0.7;">Adhan: ${time12h}</h4>
+                                  <h4 class="mb-0 text-muted" style="opacity: 0.7;">Iqamah: ${prayer === "Maghrib" ? iqamah(militarytime , 5) : iqamah(militarytime , 10)}</h4>
+                              </div>
+                          </div>
+                        </div>
+                    `;
+                    topBarHTML += `<small class="me-3">${prayer}: ${time12h}</small>`;
+                });
+                
+                if (prayerTimesContainer) {
+                    prayerTimesContainer.innerHTML = cardsHTML;
+                }
+                
+                if (topBarPrayerTimes) {
+                    topBarPrayerTimes.innerHTML = topBarHTML;
+                }
 
-    fixedContentPos: false
-  });
+                // Reinitialize WOW.js for new elements
+                new WOW().init();
+            })
+            .catch(error => {
+                console.error('Error fetching prayer times:', error);
+                if (prayerTimesContainer) {
+                    prayerTimesContainer.innerHTML = '<div class="col-12"><p>Unable to load prayer times. Please try again later.</p></div>';
+                }
+                if (topBarPrayerTimes) {
+                    topBarPrayerTimes.innerHTML = '<small>Prayer times unavailable</small>';
+                }
+            });
+    }
 
+    // Function to convert 24-hour format to 12-hour format
+    function convertTo12HourFormat(time24h) {
+        const [hour, minute] = time24h.split(':');
+        let hour12 = parseInt(hour, 10) % 12;
+        hour12 = hour12 === 0 ? 12 : hour12; // Convert '00' to '12'
+        const amPm = parseInt(hour, 10) >= 12 ? 'PM' : 'AM';
+        return `${hour12}:${minute} ${amPm}`;
+    }
 
-
-
+    // Call the function when the page loads
+    $(document).ready(function() {
+        fetchAndDisplayPrayerTimes();
+    });
 
 })(jQuery);
-
